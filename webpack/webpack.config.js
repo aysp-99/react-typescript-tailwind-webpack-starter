7b0018921b125e7d323e776025831f3470ddef83
Asfiya Shaikh <aysp.1999@gmail.com>
2021-12-06 22:23:57 +0530

    Added loaders for tailwindcss support

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -20,7 +20,11 @@ module.exports = {
             {
                 test: /\.css$/,
                 exclude: /node_modules/,
-                use: ['style-loader', 'css-loader'],
+                use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    'postcss-loader' 
+                ],
             },
         ],
     },
